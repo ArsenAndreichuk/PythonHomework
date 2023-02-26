f9d7488779643abac437aa1ee40793b3f4b1c5d0
ArsenAndreichuk <arsen.andreichuk@gmail.com>
2023-02-26 21:57:26 +0200

    домашня робота

---
+++ Lesson2/task2.py
@@ -0,0 +1,7 @@
+# hello world
+print("hello world")
+# 1^2^3^4^5^hello word
+print(1, 2, 3, 4, 5, "hello word", sep='^')
+# привіт світ! як справи?
+print("привіт світ", end='! ')
+print("як справи?")
